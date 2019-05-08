add272d6325e8f0d730ad368c564f10eebc305d9
EmbraceLife <1227561934@qq.com>
2019-05-09 05:52:32 +0800

    label_from_folder

---
+++ my_workstation/src_fastai_ex/exp.py
@@ -0,0 +1,15 @@
+from fastai.vision import *
+path_data = untar_data(URLs.MNIST_TINY); path_data.ls()
+il = ImageList.from_folder(path_data); il
+sd = il.split_by_folder(train='train', valid='valid'); sd
+ll = sd.label_from_folder()
+ll.train
+ll.train.__repr__()
+ll.train.y.classes
+
+
+ll_train = sd.train.label_from_folder(from_item_lists=True)
+ll_train.__class__
+ll_train.y.items
+
+ll_train.__repr__()
