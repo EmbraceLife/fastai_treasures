fa765ba078d7725bd177b6f4d3355cf7116154fd
EmbraceLife <1227561934@qq.com>
2019-05-12 09:25:55 +0800

    update

--- my_workstation/src_fastai_ex/exp.py
+++ my_workstation/src_fastai_ex/exp.py
@@ -5,13 +5,8 @@ sd = il.split_by_folder(train='train', valid='valid'); sd
 func=lambda o: (o.parts if isinstance(o, Path) else o.split(os.path.sep))[-2]
 ll = sd.label_from_func(func)
 ll = sd.label_from_folder()
-ll.train
-ll.train.__repr__()
-ll.train.y.classes
-
-
-ll_train = sd.train.label_from_folder(from_item_lists=True)
-ll_train.__class__
-ll_train.y.items
-
-ll_train.__repr__()
+ll.train.y[-3]
+items = ll.train.items
+cl = CategoryList(items)
+cl.filter_missing_y
+cl.__repr__()
