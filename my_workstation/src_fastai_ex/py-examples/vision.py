8c30fe0e00caea33560d9d5947602badf2788ec2
EmbraceLife <1227561934@qq.com>
2019-05-17 16:33:37 +0800

    more tools

--- my_workstation/src_fastai_ex/py-examples/vision.py
+++
@@ -1,5 +0,0 @@
-from fastai.vision import *
-import fastai.vision as fv 
-# how many classes and functions are attached to `fv`? see `fv.` tab twice
-# how to such attachment made possible? see `fv` source 
-path_data = untar_data(URLs.MNIST_TINY)
