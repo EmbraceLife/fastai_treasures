639e4416d6d0ac0e4194389b392da574173f6ff5
EmbraceLife <1227561934@qq.com>
2019-06-14 08:18:28 +0800

    core done again

--- my_workstation/my-v2/core.add_props.py
+++ my_workstation/my-v2/core.add_props.py
@@ -1,28 +1,34 @@
 from local.test import *
 from local.imports import *
 from local.notebook.showdoc import show_doc
-###############
-
-# `add_props(f, n=2)`
-# = add properties to a class, `n` set for number of properties
-# = properties difference are based on `i` from `i in range(n)`, and
-# = `partial(f, i)` => if `f` is lambda then must have two args `x` and `i`
 
 def add_props(f, n=2):
-    "Create properties passing each of `range(n)` to f"
+    """
+    purpose:
+    1. sometimes properties of a class are very similar in nature
+    2. i.e., they follow the same structure but in different values
+    3. to be specific, they are constructed by the same `f` but with different `i` values
+    4. the `i` value difference can be described as `for i in range(n)`,
+    5. where `n` refers to the number of properties, default to 2.
+    """
     return (property(partial(f,i)) for i in range(n))
 
-def examples():
 
-    class _T(): a,b = add_props(lambda i,x:i*2)
+class _T(): a,b = add_props(lambda i,x:i*2)
 
-    t = _T()
-    test_eq(t.a,0)
-    test_eq(t.b,2)
+t = _T()
+test_eq(t.a,0)
+test_eq(t.b,2)
 
-    class _T(): a,b,c,d = add_props(lambda i,x:i*2, n=4)
-    t = _T()
-    test_eq(t.a,0)
-    test_eq(t.b,2)
-    test_eq(t.c,4)
-    test_eq(t.d,6)
+class _T(): a,b,c,d = add_props(lambda i,x:i*2, n=4)
+t = _T()
+test_eq(t.a,0)
+test_eq(t.b,2)
+test_eq(t.c,4)
+test_eq(t.d,6)
+
+# more realistic usage examples
+def more_realistic_examples():
+    xt, yt = add_props(lambda i,x:x.tfms[i])
+    train_dl, valid_dl = add_props(lambda i,x: x[i])
+    train_ds, valid_ds = add_props(lambda i,x: x[i].dataset)
--- my_workstation/my-v2/core.make_cross_image.py
+++ my_workstation/my-v2/core.make_cross_image.py
@@ -1,14 +1,16 @@
 from local.test import *
 from local.imports import *
 from local.notebook.showdoc import show_doc
-####################
-# `make_cross_image(bw=True)`
-# `bw=True` = black and white cross image
-# `bw=False` = color cross image
-# Not sure of color image logic behind
 
 def make_cross_image(bw=True):
-    "Create a tensor containing a cross image, either `bw` (True) or color"
+    """
+    oneliner: how to create a tensor for black-white cross image or color make_cross_image
+
+    purpose:
+    1. to create an image of bw or color cross, we need a tensor first;
+    2. by change values in the tensor to add pattern or color
+    3. permutate to change the dim position of channels is essential in creating images
+    """
     if bw:
         im = torch.zeros(5,5)
         im[2,:] = 1.
@@ -18,9 +20,14 @@ def make_cross_image(bw=True):
         im[0,2,:] = 1.
         im[1,:,2] = 1.
     return im
-# %%
 make_cross_image()
 plt.imshow(make_cross_image(), cmap="Greys");
 make_cross_image(False)
-make_cross_image(False).permute(1,2,0)
+make_cross_image(False).shape
+make_cross_image(False).permute(1,2,0).shape
 plt.imshow(make_cross_image(False).permute(1,2,0));
+###########################################################################
+# what does permute actually do to a tensor
+show_doc(make_cross_image(False).permute)
+make_cross_image(False)[0,:,:]
+make_cross_image(False).permute(1,2,0)[:,:,0]
