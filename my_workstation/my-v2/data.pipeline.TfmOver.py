df95ef64274790605007f4b6036172a430bbfbf6
EmbraceLife <1227561934@qq.com>
2019-06-16 21:43:40 +0800

    TfmOver improved

--- my_workstation/my-v2/data.pipeline.TfmOver.py
+++ my_workstation/my-v2/data.pipeline.TfmOver.py
@@ -2,62 +2,37 @@ from local.imports import *
 from local.test import *
 from local.core import *
 from local.notebook.showdoc import show_doc
-##################
+###############################################################
 from local.data.pipeline import Transform, Pipeline, Item, TfmdList, _set_tupled
 
-
+###############################################################
 class TfmOver(Transform):
     """
     "Create tuple containing each of `tfms` applied to each of `o`"
 
     purpose:
-    - to store all tfms used?
-    - therefore, such class is a subclass of Transform
+    - why do we need `TfmOver` after `Pipeline`, `TfmdList`?
+    0. sometimes, we want multiple pipelines to work together
+    1. therefore, we want to store multiple pipelines together
+    2. we want to present the effect of different pipelines
     """
     def __init__(self, tfms=None):
         """
-        purpose:
-        - how do we create an instance of this TfmOver?
-        - first, we need tfms of course
-        - then, we can define active tfms, assoc,
-        - convert normal tfms to pipeline
+        oneliner:
+        - TfmOver.__init__ is to create and store a list of pipelines as its `tfms`
 
-        steps:
-        - if tfms args are None, make it a list of [None]
-        - define:
-            - `self.activ` = None,
-            - `self.tfms` turned into pipeline
-            - `self.assoc` = Item
+        purpose:
+        - what should be inside `TfmOver`?
+            1. if you don't provide `tfms`, then this func will provide `tfms=[None]`
+            2. it also provides `self.activ = None`
+            3. finally get the pipeline ready by
+                - `self.tfms = L(tfms).mapped(Pipeline)`
         """
         if tfms is None: tfms = [None]
-        self.activ,self.assoc = None,Item
-        tfmsL = L(tfms)
-        self.tfms = tfmsL.mapped(Pipeline)
+        self.activ,self.tfms = None,L(tfms).mapped(Pipeline)
 
-    def __call__(self, o, *args, **kwargs):
-        """
-        "List of output of each of `tfms` on `o`"
-
-        """
-        if self.activ is not None:
-            return self.tfms[self.activ](o[self.activ], *args, **kwargs)
-        return [t(p, *args, **kwargs) for p,t in zip(o,self.tfms)]
-
-    def show(self, o, ctx=None, **kwargs):
-        "Show result of `show` from each of `tfms`"
-        for p,t in zip(o,self.tfms): ctx = t.show(p, ctx=ctx, **kwargs)
-        return ctx
-
-    def decode(self, o, **kwargs): return [t.decode(p, **kwargs) for p,t in zip(o,self.tfms)]
-
-    def __repr__(self): return f'TfmOver({self.tfms})'
-
-    def setups(self, o=None):
-        "Setup each of `tfms` independently"
-        for i,tfm in enumerate(self.tfms):
-            self.activ = i
-            tfm.setup(o)
-        self.activ=None
+    @property
+    def assoc(self): return self.tfms.attrgot('assoc')
 
     @classmethod
     def piped(cls, tfms=None, final_tfms=None):
@@ -68,9 +43,134 @@ class TfmOver(Transform):
 
     xt,yt = add_props(lambda i,x:x.tfms[i])
 
-negtfm = lambda: Transform(operator.neg, decodes=operator.neg)
-floattfm = lambda: Transform(float,decodes=int,assoc=Item)
+tp = TfmOver(tfms=[operator.neg, float])
+tp.activ
+tp.tfms # not setup yet
+# tp.setups()
+# tp.tfms
+###############################################################
+@patch
+def setups(cls:TfmOver, o=None):
+    """
+    oneliner:
+    - "Setup each of `tfms` independently"
 
+    purpose:
+    - setup is a must
+    """
+    for i,tfm in enumerate(cls.tfms):
+        cls.activ = i
+        tfm.setup(o)
+    cls.activ=None
+
+tp = TfmOver(tfms=[[operator.neg, float]])
+tp.activ
+tp.tfms
+tp.tfms[0]._tfms[1].prev
+tp.setups()
+tp.tfms
+isinstance(tp.tfms[0], Pipeline)
+tp.tfms[0]
+tp.tfms[0].tfms[1].prev
+###############################################################
+@patch
+def __call__(cls:TfmOver, o, *args, **kwargs):
+    """
+    oneliner:
+    - "List of output of each of `tfms` on `o`"
+
+    purpose:
+    - we do __call__ to create the effect of each pipeline on data `o`
+        1. when `cls.active` is None, we apply each pipeline to each item of `o`
+        2. when `cls.active` is 0 for example, we apply the first pipeline to the first itme of `o`
+    """
+    if cls.activ is not None: return cls.tfms[cls.activ](o[cls.activ], *args, **kwargs)
+    return [t(p, *args, **kwargs) for p,t in zip(o,cls.tfms)]
+
+tp = TfmOver(tfms=[[operator.neg, float], [operator.neg]])
+tp.setups()
+tp([3,3])
+
+###############################################################
+@patch
+def show(cls:TfmOver, o, ctx=None, **kwargs):
+    """
+    purpose:
+    - we also need to see all pipelines' decoding and display original states
+    - just like apply each pipeline to each item in `o`, Here
+    - we decode and display each pipeline for each item in `o`
+
+    Note:
+    - to `show`, we need both `encodes` and `decodes`
+    - also we need the very first tfm to have `assoc`
+    """
+    for p,t in zip(o,cls.tfms): ctx = t.show(p, ctx=ctx, **kwargs)
+    return ctx
+
+negtfm = Transform(operator.neg,decodes = operator.neg, assoc=Item)
+fltfm = Transform(float, decodes=int)
+tp = TfmOver(tfms=[[negtfm, fltfm], [negtfm]])
+tp.setups()
+t = tp([3,3]); t
+tp.tfms
+tp.show(t) # always use pdb to see the inner working
+
+
+###############################################################
+@patch
+def decode(cls:TfmOver, o, **kwargs):
+    """
+    purpose:
+    - match each item of `o` with each pipeline in `cls.tfms`
+    - let each pipeline decode each item
+    """
+    return [t.decode(p, **kwargs) for p,t in zip(o,cls.tfms)]
+
+negtfm = Transform(operator.neg,decodes = operator.neg)#, assoc=Item)
+fltfm = Transform(float, decodes=int)
+tp = TfmOver(tfms=[[negtfm, fltfm], [negtfm]])
+tp.setups()
+t = tp([3,3]); t
+tp.show(t)
+tp.decode(t) # always use pdb to see the inner working
+
+
+###############################################################
+@patch
+def __repr__(cls:TfmOver):
+    """
+    purpose:
+    - when printing `TfmOver`, we just want to see all the pipelines
+    - Note: always `setups()` first
+    """
+    return f'TfmOver({cls.tfms})'
+
+negtfm = Transform(operator.neg,decodes = operator.neg)#, assoc=Item)
+fltfm = Transform(float, decodes=int)
+tp = TfmOver(tfms=[[negtfm, fltfm], [negtfm]])
+tp.setups()
+tp
+
+###############################################################
+# @property
+# def assoc(self): return self.tfms.attrgot('assoc')
+
+negtfm = Transform(operator.neg,decodes = operator.neg, assoc=Item)
+fltfm = Transform(float, decodes=int)
+tp = TfmOver(tfms=[[negtfm, fltfm], [negtfm]])
+tp.setups()
+tp.assoc
+
+###############################################################
+# @classmethod
+# def piped(cls, tfms=None, final_tfms=None):
+#     "`Pipeline` that duplicates input, then maps `TfmOver` over `tfms`, optionally followed by any `final_tfms`"
+#     tfms = L(ifnone(tfms,[None]))
+#     init_tfm = partial(replicate,match=tfms)
+#     return Pipeline([init_tfm,cls(tfms)] + _set_tupled(final_tfms))
+
+############## important example #####################
+mk_class('negtfm',   sup=Transform, encodes=operator.neg, decodes=operator.neg)
 class _TNorm(Transform):
     assoc=Item
     def __init__(self): self.m,self.s = 0,1
@@ -81,19 +181,20 @@ class _TNorm(Transform):
         self.m,self.s = its.mean(),its.std()
 
 items = [1,2,3,4]
-to = TfmOver(tfms=negtfm())
-to.setups()
-to(items)
-dt(to)
-TfmOver.piped([negtfm(), [negtfm(),_TNorm()]])
+ps = TfmOver.piped([negtfm(), [negtfm(),_TNorm()]])
+ps._tfms
 tl = TfmdList(items, TfmOver.piped([negtfm(), [negtfm(),_TNorm()]]))
+tl
+# TfmdList: (#4) [1,2,3,4]
+# tfms - [functools.partial(<function replicate at 0x1221898c8>, match=(#2) [<class 'local.core.negtfm'>,(#2) [<class 'local.core.negtfm'>,<class '__main__._TNorm'>]]), TfmOver((#2) [[<class 'local.core.negtfm'>],[<class 'local.core.negtfm'>, <class '__main__._TNorm'>]])]
 
 x,y = zip(*tl)
 test_close(tensor(y).mean(), 0)
 test_close(tensor(y).std(), 1)
 test_eq(x, [-1,-2,-3,-4])
 test_stdout(lambda:tl.show_at(1), 'tensor(-2.)')
-
+test_eq(tl.tfm.assoc, [None,Item])
+# %%
 # Create a "batch"
 b = list(zip(*tl))
 bd = tl.decode_batch(b)
@@ -109,3 +210,10 @@ print('bd',bd)
 tp = TfmOver()
 tp.setup()
 test_eq(tp([1]), [1])
+# %% markdown
+# ## Export -
+# %%
+#hide
+from local.notebook.export import notebook2script
+notebook2script(all_fs=True)
+# %%
