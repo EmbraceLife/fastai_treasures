99a85a08544f923f06b164158808f9412d849e69
EmbraceLife <1227561934@qq.com>
2019-06-22 08:43:39 +0800

    get, mk, wrap class

---
+++ my_workstation/my-v2/core.mk_class.py
@@ -0,0 +1,52 @@
+from local.imports import *
+from local.test import *
+from local.notebook.showdoc import show_doc
+from local.core import *
+
+show_doc(mk_class)# check latest source and test
+
+def mk_class(nm, *fld_names, sup=None, doc=None, funcs=None, **flds):
+    """
+    "Create a class using `get_class` and add to the caller's module"
+
+    why mk_class(...)
+    - `get_class(...)` can create a new class with specified attrs for us
+    - but what if we want to add this class onto the caller's module
+    - we use `mk_class` to create and add onto module altogether
+
+    how to use mk_class(...)
+    - the same as get_class(...)
+    - but does not return anything
+
+    Note:
+    - get_class(...) returns a new class
+    - add onto module means you can directly access the class, see example below
+    """
+    stk = inspect.stack()[1]
+    mod = ifnone(inspect.getmodule(stk[0]), sys.modules['__main__'])
+    res = get_class(nm, *fld_names, sup=sup, doc=doc, funcs=funcs, **flds)
+    setattr(mod, nm, res)
+
+
+############################################
+# Any kwargs will be added as class attributes, and sup is an optional (tuple of) base classes.
+mk_class('_t', a=1, sup=GetAttr)
+t = _t() # __init__ without any *args or **kwargs
+test_eq(t.a, 1)
+assert(isinstance(t,GetAttr))
+
+################ reminder of `get_class()` content
+# A __init__ is provided that sets attrs for any kwargs,
+# and for any args (matching by position to fields),
+# along with a __repr__ which prints all attrs.
+# The docstring is set to doc.
+# You can pass funcs which will be added as attrs with the function names.
+
+def foo(self): return 1
+mk_class('_t', 'a', sup=GetAttr, doc='test doc', funcs=foo)
+t = _t(3, b=2, _xtra=['lower'], default="GDP") # __init__ with args and kwargs
+t.a
+t.b
+t.lower()
+t.foo()
+t.__doc__
---
+++ my_workstation/my-v2/core.wrap_class.py
@@ -0,0 +1,35 @@
+from local.imports import *
+from local.test import *
+from local.notebook.showdoc import show_doc
+from local.core import *
+
+show_doc(wrap_class)
+###############################################################################
+def wrap_class(nm, *fld_names, sup=None, doc=None, funcs=None, **flds):
+    """
+    why wrap_class(...):
+    - we have `get_class(...)` to create a new class with specified attrs
+    - we have `mk_class(...)` to create and add new class onto caller's module
+    - we have `@patch` to add a new method to a class anywhere in an experiment
+    - why not have @wrap_class to create, add onto module and create a method whenever and wherever we like very super fast?
+
+    how to use wrap_class(...)?
+    - `@wrap_class(...)`: as if we are using `mk_class(...)`
+    - immediately create a method as if we were writing a class and its methods
+
+    how to create wrap_class(...)?
+    - making a decorator to wrap a method with `mk_class(...)`
+    """
+    def _inner(f):
+        mk_class(nm, *fld_names, sup=sup, doc=doc, funcs=L(funcs)+f, **flds)
+        return f
+    return _inner
+
+# how to create a class and a method in two lines of code
+@wrap_class('_t', a=2) # say you want a class called '_t' with attr 'a=2'
+def bar(self,x): return x+1 # say you want this class has a method `bar`
+
+t = _t()
+t.a
+t.bar(3)
+t
